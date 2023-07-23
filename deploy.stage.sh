90560dfdb2e20d2d1d424d6af1f43f7b23ddc04c
izica <izica@redmibook>
2023-07-24 06:07:51 +0700

    Update workspace.xml and deploy.stage.sh

--- deploy.stage.sh
+++ deploy.stage.sh
@@ -2,5 +2,5 @@
 cd ./.docker
 docker-compose -f docker-compose.local.yml build nextjs
 docker-compose -f docker-compose.local.yml stop
-docker-compose -f up -d
+docker-compose -f docker-compose.local.yml up -d
 docker image prune -a -f
