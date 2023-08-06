eb1d9939af46c98e291fb3c1972f1c349fbb994f
izica <izica@redmibook>
2023-08-07 00:17:14 +0700

    Update workspace.xml, index.scss, and index.tsx

--- shared/components/catalog/CCatalogProductsViewed/index.tsx
+++ shared/components/catalog/CCatalogProductsViewed/index.tsx
@@ -2,9 +2,11 @@
 
 import { observer } from "mobx-react-lite";
 import React from "react";
+import { CCatalogProductsSlider } from "shared/components/catalog";
 import { useAsyncEffect, useObservable } from "shared/hooks";
 
 import { CatalogProductModel } from "shared/models";
+import { catalogProductsQuery } from "shared/queries/main";
 import { CatalogService } from "shared/services";
 import { UiPage } from "shared/ui";
 
@@ -17,11 +19,18 @@ export const CCatalogProductsViewed = observer(() => {
     });
 
     useAsyncEffect(async () => {
-        store.set("isInitialized", true);
         const viewed = await CatalogService.getViews();
-        console.log(viewed)
+        if (viewed.length === 0) {
+            return;
+        }
+        const { isSuccess, data } = await catalogProductsQuery({
+            id: viewed
+        });
+        if (isSuccess && data) {
+            store.set("catalogProducts", data.items.map(item => new CatalogProductModel(item)));
+            store.set("isInitialized", true);
+        }
     }, [store])
-
 
     if (!store.isInitialized) {
         return null;
@@ -29,7 +38,7 @@ export const CCatalogProductsViewed = observer(() => {
 
     return (
         <UiPage.Section title={'Вы смотрели'}>
-
+            <CCatalogProductsSlider catalogProducts={store.catalogProducts}/>
         </UiPage.Section>
     )
 });
